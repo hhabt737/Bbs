a1db47d7c74c376d5331d520dfdf633f1682ee38
HyunheeKim <hhabt737@naver.com>
2020-03-07 03:02:35 +0900

    Create bulletin board

---
+++ src/bbs/BbsDAO.java
@@ -0,0 +1,36 @@
+package bbs;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class BbsDAO {
+	
+	private Connection conn;
+	private ResultSet rs;
+	
+	public BbsDAO() {
+		try {
+			String dbURL = "jdbc:mysql://localhost:3306/BBS?characterEncoding=UTF-8&serverTimezone=UTC";
+			String dbID = "root";
+			String dbPassword = "root";
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String getDate() {
+		String SQL = "SELECT NOW()";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "";  // 데이터베이스 오류
+	}
+	
+}
